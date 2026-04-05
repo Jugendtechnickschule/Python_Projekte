ab439f21ba76f7ca9889e5fa7d7db46c3ba5ff7d
Lennard Holzinger <jts@mint-main-tauber.de>
2026-04-05 20:18:39 +0200

    neue übung

---
+++ Übungen/Übumg_variablen, importieren.py
@@ -0,0 +1,37 @@
+"""übung von variablen, importieren von modulen und funktionen"""
+
+# importieren von modulen
+import time
+# nur die benötigten module importieren
+from random import randint
+
+# variablen
+name = "Max"
+alter = 15
+ist_schule = True
+list = [1, 2, 3, 4, 5]
+
+# ausgabe von variablen
+print(name)
+print(alter)
+print(ist_schule)
+print(list)
+
+# verwenden von funktionen
+
+
+
+
+
+
+
+print(time.time())
+print(randint(1, 10))
+
+
+
+
+
+
+
+
