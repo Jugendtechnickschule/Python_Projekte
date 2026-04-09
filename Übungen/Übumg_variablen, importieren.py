fc816c6460558d69b3aeae1d1dd0c15661f341b6
Lennard Holzinger <jts@mint-main-tauber.de>
2026-04-09 20:43:24 +0200

    einfache erweiterungen

--- Übungen/Übumg_variablen, importieren.py
+++ Übungen/Übumg_variablen, importieren.py
@@ -5,20 +5,32 @@ import time
 # nur die benötigten module importieren
 from random import randint
 
+#localtime() gibt die aktuelle zeit zurück
+print(time.localtime())
+
 # variablen
 name = "Max"
 alter = 15
 ist_schule = True
 list = [1, 2, 3, 4, 5]
+randzahl = randint(1, 100)
 
 # ausgabe von variablen
 print(name)
 print(alter)
 print(ist_schule)
 print(list)
+print(randzahl)
 
 # verwenden von funktionen
 
+def begruessung(name):
+    print("Hallo " + name + "!")
+
+
+# for-schleife
+for i in range(10):
+    print(i)
 
 
 
